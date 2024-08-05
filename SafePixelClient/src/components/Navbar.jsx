8f1625ac50593bf33da3698ef780dffb20b3e686
Alok-2003 <alok953280@gmail.com>
2024-08-05 23:05:03 +0530

    Get image and navbar componnent

---
+++ SafePixelClient/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import ConnectedAccount from "./ConnectedAccount";
+const Navbar = () => {
+    return (  
+        <div className=" w-full h-[80px] bg-gray-600 flex justify-center items-center ">
+         <ConnectedAccount />
+        </div> 
+    );
+}
+ 
+export default Navbar;
